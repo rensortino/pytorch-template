d7774e092fb80d8573610e1ec55aa4b239fc727f
Renato Sortino <53495252+rensortino@users.noreply.github.com>
2022-10-06 10:44:12 +0200

    Added resblock

---
+++ modules/resblock.py
@@ -0,0 +1,15 @@
+from torch import nn
+
+class ResBlock(nn.Module):
+    def __init__(self, chan):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(chan, chan, 3, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(chan, chan, 3, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(chan, chan, 1)
+        )
+
+    def forward(self, x):
+        return self.net(x) + x
