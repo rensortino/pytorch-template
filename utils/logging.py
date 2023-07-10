55d56cf95b78c71f893d8479a20f0a421a841bf0
Renato Sortino <rensortino@gmail.com>
2023-07-10 15:43:40 +0200

    Refactored logger

--- utils/logging.py
+++ utils/logging.py
@@ -1,14 +1,13 @@
 from datetime import datetime
 from pathlib import Path
-# from torchsummary.torchsummary import summary
 import sys
 import torch
 import wandb
-from logzero import logger as lz_logger
+from logzero import logger
 import logzero
+from torchvision.utils import save_image
 
 
-# TODO Remove wandb_run and artifacts 
 class Logger:
 
     def __init__(self, output_dir, run_name, on_wandb):
@@ -19,9 +18,7 @@ class Logger:
         self._set_dirs()
 
         logzero.logfile(self.out_dir / Path('output.log'))
-        if on_wandb:
-            wandb.login()
-            wandb.init(name=run_name)
+        
         
         self._log_command()
 
@@ -30,6 +27,7 @@ class Logger:
         formatted_date = f'{current_date.year}-{current_date.month}-{current_date.day}-{current_date.hour}h{current_date.minute}'
         test_name = f'{formatted_date}'
         self.out_dir = self.out_dir / self.run_name / Path(test_name)
+        self.info(f'Saving output in: {self.out_dir}')
         self.out_dir.mkdir(parents=True, exist_ok=True)
 
     def _log_command(self):
@@ -38,13 +36,11 @@ class Logger:
             out.write(cmd_args)
             out.write('\n')
 
-    def log_image(self, pil_img, phase, title, step):
-        pil_img.save(self.out_dir / Path(f'{title}.png'))
-        if self.on_wandb:
-            wandb.log({f'{phase}/{title}': wandb.Image(pil_img), 'epoch':step})
+    def save_image(self, img, title, step):
+        save_image(img, self.out_dir / Path(f'{step}_{title}.png'))
 
     def log_metric(self, title, metric, step):
-        lz_logger.info(f'Epoch [{step}] - {title}: {metric}')
+        logger.info(f'Epoch [{step}] - {title}: {metric}')
         if self.on_wandb:
             wandb.log({title: metric, 'epoch': step})
 
@@ -54,14 +50,14 @@ class Logger:
             wandb.log({title: lr, "epoch": epoch})
 
     def warning(self, text):
-        lz_logger.warning(text)
+        logger.warning(text)
 
     def info(self, text):
-        lz_logger.info(text)
+        logger.info(text)
 
     def save_ckpt(self, ckpt, path='checkpoint.pt'):
         path = Path(path)
-        lz_logger.info(f'Saving {path} in {self.out_dir}')
+        logger.info(f'Saving {path} in {self.out_dir}')
         torch.save(ckpt, self.out_dir / path)
         if self.on_wandb:
             wandb.save(str(self.out_dir / path))
@@ -73,9 +69,9 @@ def get_lr(optimizer):
 
 
 if __name__ == '__main__':
-    logger = Logger('logs_test', 'Test logging')
-    logger.log_metric('Test metric', 0.5, 1, on_wandb=False)
+    logger = Logger('logs_test', 'Test logging', on_wandb=False)
+    logger.log_metric('Test metric', 0.5, 1)
     logger.info('This is an info message')
     logger.warning('This is a warning')
     ckpt = torch.randn(64, 3, 256, 256)
-    logger.save_ckpt(ckpt, 'test.pt', on_wandb=False)
+    logger.save_ckpt(ckpt, 'test.pt')
